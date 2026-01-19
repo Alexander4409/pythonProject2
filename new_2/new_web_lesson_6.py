d8cfc0d920281b529b489b857184b3ee69b61e2e
Alexander <aleksandr.tailakov@mail.ru>
2026-01-19 21:41:55 +0700

    lesson 6 for new_web_2

---
+++ new_2/new_web_lesson_6.py
@@ -0,0 +1,15 @@
+#Зарегистрироваться в
+# гит хаб - https://github.com/Alexander4409
+# гит флик - https://gitflic.ru/user/aleksandr4409
+# материалы для рабы с системой контроля версий - https://habr.com/ru/companies/yandex_praktikum/articles/700708/
+# Гитхаб + пайчарм - https://www.youtube.com/watch?v=IH0W5bm4orc
+# дать ссылку на скачивание пайчарм и питон гайд по установке пайчарм и питон -
+# скачивание пайчарм https://gb.ru/manualpycharmcommunity
+# установка пайчарм - https://pythonru.com/baza-znanij/poshagovaja-ustanovka-pycharm
+#
+# def doble(num):
+#     return  num * 2
+#
+# num_1 = int(input("Enter some num"))
+#
+# print(doble(num_1))
