b788ab153499794e4f95c461b785f366045e5335
Alexander <aleksandr.tailakov@mail.ru>
2026-01-20 21:34:10 +0700

    Add lesson 6 for new_web

---
+++ new/new_web_lesson_6.py
@@ -0,0 +1,35 @@
+#Зарегистрироваться в
+# гит хаб - https://github.com/Alexander4409
+# гит флик - https://gitflic.ru/user/aleksandr4409
+# материалы для рабы с системой контроля версий - https://habr.com/ru/companies/yandex_praktikum/articles/700708/
+# Гитхаб + пайчарм - https://www.youtube.com/watch?v=IH0W5bm4orc
+# дать ссылку на скачивание пайчарм и питон гайд по установке пайчарм и питон -
+# скачивание пайчарм https://gb.ru/manualpycharmcommunity
+# установка пайчарм - https://pythonru.com/baza-znanij/poshagovaja-ustanovka-pycharm
+
+# def doble(num):
+#     return num * 2
+#
+# user_input = int(input("Enter some num"))
+#
+# print(doble(user_input))
+
+#lambda выражения
+#Формальная запись - lambda [параметры] : инструкция
+
+# message = lambda: print("hello")
+#
+# message()  # hello
+#
+# #Аналог def
+# def hi():
+#     print("hello")
+#
+# hi()
+
+
+# работа с параметрами
+
+# double = lambda num: num * 2
+#
+# print(double(4))
