ad4aea5e83d13a2fe9bc219f5ef5f057235bbb3e
Alexander <aleksandr.tailakov@mail.ru>
2026-02-06 21:33:53 +0700

    Add lesson 8 for deep

---
+++ deep/deep_web_lesson_8.py
@@ -0,0 +1,38 @@
+#ООП
+# class Person:
+#     def __init__(self,name,age):
+#         #Атрибуты класса
+#         self.name = name
+#         self.age = age
+#         print(f"Account with name {self.name} created!")
+#
+#
+#         #Метод класса
+#     def walk(self):
+#         print("I'm go for a walk")
+#
+#     def display_info(self):
+#         print(f'Name: {self.name} Age: {self.age}')
+#         #Деструктор класса
+#     def __del__(self):
+#         print(f"Account with name: {self.name} deleted!")
+#
+#
+#
+# def created_person():
+#     tom = Person("Tom",43)
+#
+#
+#
+# created_person()
+# print("Program ended")
+
+#Прямоугольник, треугольник, круг: Напишите классы
+# принимающий в конструкторе длину и ширину или радиус.
+# Реализуйте метод area для вычисления площади и perimeter для периметра.
+# Рефлексия и думать над проектами !!!
+
+
+
+
+
