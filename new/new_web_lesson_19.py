a884597100390e133b036771c1b7321d41e456a3
Alexander <aleksandr.tailakov@mail.ru>
2026-04-21 21:30:59 +0700

    Add new lesson 19

---
+++ new/new_web_lesson_19.py
@@ -0,0 +1,53 @@
+#ООП
+#объект - имеет 3 ключевых показателя, тип, идентификатор, значение
+# num = 14
+#
+# print(id(num))
+#
+# num_2 = 15
+# print(hex(id(num_2)))
+from tkinter.font import names
+
+
+# класс - это шаблон благодаря которому мы можем перенести некоторые
+# физические свойства объекта в цифровой мир
+# class Person:
+#     pass
+# # tom и bob это экземпляры(объекты) класса которые получены из класса Person
+# tom = Person()
+# bob = Person()
+#
+# print(type(tom))
+
+class Human:
+    #метод класса
+    def __init__(self,name,age):
+        # Атрибуты класса
+        self.name = name
+        self.age = age
+
+    def say_hi(self):
+        print("Hello")
+
+
+
+# Объекты класса (экземпляры)
+tom = Human("Tom",23)
+bob = Human("Bob", 24)
+
+bob.say_hi()
+
+# #Обращение к атрибутам экземпляров класса
+# print(tom.age)
+# print(bob.age)
+# print(tom.name)
+#
+# tom.age = 37
+# print(tom.age)
+# #Динамический атрибут !!! ненада
+# tom.company = "VK"
+# print(tom.company)
+
+#https://metanit.com/python/tutorial/7.1.php
+
+
